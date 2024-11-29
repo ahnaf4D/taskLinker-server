e9bd855039df6f17421f825e272454ab09f90ef8
ahnaf4d <ahnaftahmid802@gmail.com>
2024-11-29 19:39:03 +0600

    create user and get single user api added

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const verifyToken = async (req, res, next) => {
     const token = req.headers.authorization;
     jwt.verify(token, process.env.JWT_ACCESS_TOKEN, (err, decoded) => {
         if (err) {
-            return res.status(401).send({ message: 'unauthorized access' })
+            return res.status(401).send({ success: false, message: 'unauthorized access' })
         }
         req.decoded = decoded;
         next();
@@ -70,12 +70,39 @@ async function run() {
             }
             next();
         }
-        // api code goes here
+        // Create a user record in db
+        app.post(`/api/users`, async (req, res) => {
+            try {
+                const user = req.body;
+                const query = { email: user.email };
+                const existingEmail = await usersCollection.findOne(query);
+                if (existingEmail) {
+                    return res.status(400).send({ success: false, massage: "User already exits" })
+                }
+                const result = await usersCollection.insertOne(user);
+                res.status(200).send({ success: true, result });
+            } catch (error) {
+                console.error("Error massage ", error);
+                res.status(500).send({ massage: "Internal server error" });
+            }
+        })
+        app.get(`/api/user`, verifyToken, async (req, res) => {
+            try {
+                const { email } = req.query;
+                if (!email) {
+                    return res.status(400).json({ massage: "Email query parameter is required" });
+                }
+                const user = await usersCollection.findOne({ email });
+                if (!user) {
+                    return res.status(404).json({ message: 'User not found with the provided email.' });
+                }
+                res.status(200).send({ success: true, user });
+            } catch (error) {
+                console.error('Error fetching user:', error.message);
+                res.status(500).json({ message: 'Internal Server Error' });
+            }
 
-
-
-
-
+        })
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
@@ -86,6 +113,11 @@ async function run() {
 run().catch(console.dir);
 app.get('/', (req, res) => {
     res.status(200).send({ success: true, massage: "TaskLinker Server is running" })
+})
+app.post('/api/auth', async (req, res) => {
+    const userEmail = req.body;
+    const token = jwt.sign(userEmail, process.env.JWT_ACCESS_TOKEN, { expiresIn: process.env.JWT_Expire });
+    res.send({ token });
 })
 app.listen(port, () => {
     console.log(`Server listening at port ${port}`);
